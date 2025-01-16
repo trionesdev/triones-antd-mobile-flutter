879e623409fb477506043f314a86b00acafcfe78
落雁沙 <fengxiaotx@163.com>
2025-01-17 00:45:18 +0800

    feat:init

---
+++ lib/button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
+@Category(<String>['Antd', 'Button'])
+class RawAntButton extends StatelessWidget{
+
+  final VoidCallback? onPressed;
+  final VoidCallback? onLongPress;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
