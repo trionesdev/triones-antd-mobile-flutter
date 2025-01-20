fc2052d0a3c780416360af007dc9b9f37d73f274
落雁沙 <fengxiaotx@163.com>
2025-01-20 11:03:52 +0800

    feat: input

--- lib/antd.dart
+++ lib/antd.dart
@@ -1,5 +1,5 @@
 library;
 
+export 'src/components/theme/theme_data.dart';
 export 'src/components/app/app.dart';
 export 'src/components/button/button.dart';
-export 'src/components/theme/theme_data.dart';
---
+++ lib/src/components/input/input.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class Input extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+
+class _InputState extends State<Input> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+    TextFi
+  }
+
+}
