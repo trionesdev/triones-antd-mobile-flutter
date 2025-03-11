cc3ac6f73c64ccb578eda3dc6d1083bca3902e4f
落雁沙 <fengxiaotx@163.com>
2025-03-11 18:36:18 +0800

    feat:

--- lib/src/components/list/list.dart
+++ lib/src/components/list/list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:trionesdev_antd_mobile/antd.dart';
-
 
 typedef ListItemRenderCallback = Widget Function(
     BuildContext context, dynamic item, int index);
@@ -11,20 +11,32 @@ class AntList extends StatefulWidget {
       this.separator,
       this.dataSource,
       this.itemRender,
-      this.children});
+      this.children,
+      this.style,
+      this.scrollController});
 
+  final StateStyle? style;
   final List<Widget>? children;
   final Widget? separator;
   final List<dynamic>? dataSource;
   final ListItemRenderCallback? itemRender;
+  final ScrollController? scrollController;
 
   @override
   State<StatefulWidget> createState() => _AntListState();
 }
 
-class _AntListState extends State<AntList> {
+class _AntListState extends State<AntList> with MaterialStateMixin {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    StateStyle stateStyle = _AntListStyle(context, widget);
+    stateStyle = stateStyle.merge(widget.style);
+
     List<Widget> children = [];
     if (widget.children != null) {
       children.addAll(widget.children!);
@@ -32,15 +44,28 @@ class _AntListState extends State<AntList> {
     if (widget.dataSource != null && widget.itemRender != null) {
       for (int i = 0; i < widget.dataSource!.length; i++) {
         children.add(widget.itemRender!(context, widget.dataSource![i], i));
-        if (widget.separator != null) {
+        if (widget.separator != null && i < widget.dataSource!.length - 1) {
           children.add(widget.separator!);
         }
       }
     }
-    return ListView(
-      children: children,
+    return Container(
+      decoration: stateStyle.resolve(materialStates)?.decoration,
+      padding: stateStyle.resolve(materialStates)?.computedPadding,
+      child: ListView(
+        controller: widget.scrollController,
+        children: children,
+
+      ),
     );
   }
+}
+
+class _AntListStyle extends StateStyle {
+  const _AntListStyle(this.context, this.list);
+
+  final BuildContext context;
+  final AntList list;
 }
 
 class AntListItem extends StatefulWidget {
@@ -59,10 +84,14 @@ class AntListItem extends StatefulWidget {
 
 class _AntListItemState extends State<AntListItem> {
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+  }
 
-    StateStyle style = _AntListItemStyle(context,widget);
-style = style.merge(widget.style);
+  @override
+  Widget build(BuildContext context) {
+    StateStyle stateStyle = _AntListItemStyle(context, widget);
+    stateStyle = stateStyle.merge(widget.style);
 
     List<Widget> children = [];
     if (widget.icon != null) {
@@ -79,8 +108,8 @@ style = style.merge(widget.style);
         widget.onTap?.call();
       },
       child: Container(
-        decoration: style.resolve(const <WidgetState>{})?.decoration,
-        padding: style.resolve(const <WidgetState>{})?.computedPadding,
+        decoration: stateStyle.resolve(const <WidgetState>{})?.decoration,
+        padding: stateStyle.resolve(const <WidgetState>{})?.computedPadding,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           spacing: 2,
@@ -93,20 +122,18 @@ style = style.merge(widget.style);
 
 class _AntListItemStyle extends StateStyle {
   const _AntListItemStyle(this.context, this.listItem);
+
   final BuildContext context;
   final AntListItem listItem;
 
-
   @override
-  Style get style{
+  Style get style {
     return Style(
-      padding: StylePadding(
-        top: 8,
-        bottom: 8,
-        left: 8,
-        right: 8,
-      )
-    );
+        padding: StylePadding(
+      top: 8,
+      bottom: 8,
+      left: 8,
+      right: 8,
+    ));
   }
-
 }
