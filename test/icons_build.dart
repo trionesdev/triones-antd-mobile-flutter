319292867e740a84b6600d142bd5091aa846533b
落雁沙 <fengxiaotx@163.com>
2025-10-25 11:51:53 +0800

    feat: 调整icon 为常量

--- test/icons_build.dart
+++ test/icons_build.dart
@@ -8,6 +8,6 @@ Future<void> main() async {
   final jsonString = await file.readAsString();
   Map<String, dynamic> jsonMap = json.decode(jsonString);
   jsonMap['glyphs'].forEach((item) {
-    print('static final IconData ${item["name"][0].toLowerCase() + item["name"].substring(1)} = AntIconData(${item['unicode_decimal']});');
+    print('static final IconData ${item["name"][0].toLowerCase() + item["name"].substring(1)} = const AntIconData(${item['unicode_decimal']});');
   });
 }
