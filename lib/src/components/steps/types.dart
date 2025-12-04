5dec1256e502344dd8910bca85c181f0cc13c2a2
bane.shi <fengxiaotx@163.com>
2025-12-04 17:26:07 +0800

    feat: steps

--- lib/src/components/steps/types.dart
+++ lib/src/components/steps/types.dart
@@ -5,7 +5,21 @@ enum AntStepsDirection { horizontal, vertical }
 enum AntStepStatus { wait, process, finish, error }
 
 class AntStepItemStruct {
-  AntStepItemStruct({this.icon});
+  AntStepItemStruct({
+    this.icon,
+    this.iconSize,
+    this.status = AntStepStatus.process,
+    this.title,
+    this.subTitle,
+    this.child,
+    this.disabled = false,
+  });
 
+  final bool disabled;
   Widget? icon;
+  final double? iconSize;
+  final AntStepStatus status;
+  final Widget? title;
+  final Widget? subTitle;
+  final Widget? child;
 }
