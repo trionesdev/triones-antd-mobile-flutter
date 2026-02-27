0aaad0897666779f7ccbd29e390f8b4435674f02
bane.shi <fengxiaotx@163.com>
2026-02-27 11:16:02 +0800

    feat:

--- lib/src/components/tab_bar/tab_bar.dart
+++ lib/src/components/tab_bar/tab_bar.dart
@@ -244,7 +244,7 @@ class AntTabBarItemState extends State<AntTabBarItem> {
     if (tabBar?.activeColor != null) {
       return tabBar?.activeColor;
     }
-    return AntTheme.of(context).colorPrimary;
+    return AntTheme.of(context).colorPrimaryText;
   }
 
   bool get isActive {
